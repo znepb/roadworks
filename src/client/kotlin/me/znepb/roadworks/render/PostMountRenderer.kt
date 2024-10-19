1f7693a25d7e1c50d3e71de747b84277320cb4f6
Marcus Wenzel <accounts@znepb.me>
2024-10-19 16:52:16 -0400

    feat: emissive traffic lights

--- src/client/kotlin/me/znepb/roadworks/render/PostMountRenderer.kt
+++ src/client/kotlin/me/znepb/roadworks/render/PostMountRenderer.kt
@@ -1,5 +1,6 @@
 package me.znepb.roadworks.render
 
+import me.znepb.roadworks.RoadworksClient.logger
 import me.znepb.roadworks.RoadworksMain.ModId
 import me.znepb.roadworks.block.post.AbstractPostMountableBlockEntity
 import me.znepb.roadworks.block.signals.AbstractTrafficSignalBlockEntity
@@ -7,11 +8,17 @@ import me.znepb.roadworks.block.signals.SignalLight
 import me.znepb.roadworks.datagen.ModelProvider
 import me.znepb.roadworks.util.RenderUtils
 import me.znepb.roadworks.util.RenderUtils.Companion.renderModel
+import net.minecraft.block.BlockState
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.render.LightmapTextureManager
 import net.minecraft.client.render.TexturedRenderLayers
 import net.minecraft.client.render.VertexConsumer
 import net.minecraft.client.render.VertexConsumerProvider
+import net.minecraft.client.render.model.BakedQuad
 import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.util.Identifier
 import net.minecraft.util.math.Direction
+import net.minecraft.util.math.random.Random
 import org.joml.Quaternionf
 
 class PostMountRenderer(
@@ -47,19 +54,43 @@ class PostMountRenderer(
     ) {
         if(entity !is AbstractTrafficSignalBlockEntity) return
 
+        val modelLocation =
+            ModId(if (entity.getSignal(signalLight))
+                "block/signal_${signalLight.light}_on"
+            else
+                "block/signal_${signalLight.light}_off")
+
         matrices.push()
         rotateForRender()
         matrices.translate(x, y, postOffset)
-        renderModel(
-            matrices, buffer, light, overlay,
-            ModId(
-                if (entity.getSignal(signalLight))
-                    "block/signal_${signalLight.light}_on"
-                else
-                    "block/signal_${signalLight.light}_off"
-            ),
-            null
-        )
+
+        val model = MinecraftClient.getInstance().bakedModelManager.getModel(modelLocation)
+
+        if(model != null) {
+            val random = Random.create(0)
+
+            random.setSeed(42L)
+
+            val matrix = matrices.peek()
+            val quads: Iterator<*> = model.getQuads(null as BlockState?, null, random).iterator()
+            while (quads.hasNext()) {
+                val bakedquad = quads.next() as BakedQuad
+                val face = bakedquad.face
+
+                val sprite = bakedquad.sprite
+
+                val newLight =
+                    if (!sprite.contents.id.path.endsWith("signal_back") && entity.getSignal(signalLight) && face == Direction.NORTH)
+                        LightmapTextureManager.MAX_LIGHT_COORDINATE
+                    else
+                        light
+                
+                buffer.quad(matrix, bakedquad, 1F, 1F, 1F, newLight, overlay)
+            }
+        } else {
+            renderModel(matrices, buffer, light, overlay, MinecraftClient.getInstance().bakedModelManager.missingModel, direction)
+        }
+
         matrices.pop()
     }
 }
